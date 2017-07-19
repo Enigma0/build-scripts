e7a37da0eeb8ae3f87e25bf49bb7d58243f33d5f
Enigma0 <enigma0@users.noreply.github.com>
2017-07-18 19:09:19 -0500

    added daemon stops & starts

--- build_radeon_profile.sh
+++ build_radeon_profile.sh
@@ -4,9 +4,10 @@ git pull
 qmake && make
 sudo cp radeon-profile /usr/bin/radeon-profile
 
+sudo service radeon-profile-daemon stop
 cd ~/radeon-profile-daemon/radeon-profile-daemon
 git pull
 qmake-qt4 && make
 sudo cp radeon-profile-daemon /usr/bin/radeon-profile-daemon
-sudo service radeon-profile-daemon restart
+sudo service radeon-profile-daemon start
 radeon-profile &
