8fd4687a555ec204dd4d25962d2d4ecfd12394aa
Enigma <Enigma0@users.noreply.github.com>
2017-07-21 10:09:57 -0400

    removed qt4 qmake

--- build_radeon_profile.sh
+++ build_radeon_profile.sh
@@ -7,7 +7,7 @@ sudo cp radeon-profile /usr/bin/radeon-profile
 sudo service radeon-profile-daemon stop
 cd ~/radeon-profile-daemon/radeon-profile-daemon
 git pull
-qmake-qt4 && make
+qmake && make
 sudo cp radeon-profile-daemon /usr/bin/radeon-profile-daemon
 sudo service radeon-profile-daemon start
 radeon-profile &
