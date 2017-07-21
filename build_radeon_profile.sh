04977e95f43b0c4956b123e98adeeda97239ceb7
Enigma <Enigma0@users.noreply.github.com>
2017-07-21 11:04:16 -0400

    added make clean calls

--- build_radeon_profile.sh
+++ build_radeon_profile.sh
@@ -1,12 +1,14 @@
 sudo killall radeon-profile
 cd ~/radeon-profile/radeon-profile
 git pull
+make clean
 qmake && make
 sudo cp radeon-profile /usr/bin/radeon-profile
 
 sudo service radeon-profile-daemon stop
 cd ~/radeon-profile-daemon/radeon-profile-daemon
 git pull
+make clean
 qmake && make
 sudo cp radeon-profile-daemon /usr/bin/radeon-profile-daemon
 sudo service radeon-profile-daemon start
